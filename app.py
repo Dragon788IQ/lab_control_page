f48daebd13c6052dc402914396e8158088ef0a38
Johann <johann200_cardenas788@hotmail.com>
2022-12-05 12:55:43 -0600

    arreglando las imagenes intento 2

--- app.py
+++ app.py
@@ -21,15 +21,15 @@ def local_css(file_name):
 #Assets
 correo_animation_url = "https://assets10.lottiefiles.com/packages/lf20_4Ox5W3dL45.json"
 laptop_animation_url = "https://assets8.lottiefiles.com/packages/lf20_w51pcehl.json"
-imagen_1 = Image.open("imagenes/recorte_1.png")
-imagen_2 = Image.open("imagenes/recorte_2.png")
-imagen_5 = Image.open("imagenes/recorte_5.png")
+imagen_1 = Image.open("Imagenes/recorte_1.png")
+imagen_2 = Image.open("Imagenes/recorte_2.png")
+imagen_5 = Image.open("Imagenes/recorte_5.png")
 grafica_img = Image.open("imagenes/Grafica.png")
 instrucciones_img = Image.open("imagenes/instrucciones.png")
 #Imagenes de la guia de instalcion
-descarga_img = Image.open("imagenes/descarga.PNG")
-componentev2 = Image.open("imagenes/componente_v2.PNG")
-archivo_exe = Image.open("imagenes/erchivo_exe.PNG")
+descarga_img = Image.open("Imagenes/descarga.PNG")
+componentev2 = Image.open("Imagenes/componente_v2.PNG")
+archivo_exe = Image.open("Imagenes/erchivo_exe.PNG")
 
 local_css("Estilos/estilos.css")
 
